67b33f127dc9aaed37f8944a269e68d1f6ec8363
Madhu Paka <madhupaka11@gmail.com>
2025-07-11 15:28:46 +0530

    fixing issues

--- app/api/listings/[id]/route.ts
+++ app/api/listings/[id]/route.ts
@@ -1,24 +1,15 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { listingData } from '../../../../lib/listingData'
 
-
-type Context =  {
-  params: {
-    id: string;
-  }
-
-}
-
-export async function PATCH(req: NextRequest, context: Context ) {
-  const id = context.params.id ;
-  const body = await req.json()
-
+export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const body = await req.json();
   const index = listingData.findIndex(item => item?.id === id);
-
+  
   if (index === -1) {
-    return NextResponse.json({ error: 'Listing not found' }, { status: 404 })
+    return NextResponse.json({ error: 'Listing not found' }, { status: 404 });
   }
-
+  
   const updated = {
     ...listingData[index],
     ...body,
@@ -26,9 +17,8 @@ export async function PATCH(req: NextRequest, context: Context ) {
       reviewedBy: 'admin@company.com',
       reviewedAt: new Date().toISOString()
     })
-  }
-
-  listingData[index] = updated
-
-  return NextResponse.json({ message: 'Updated', listing: updated })
+  };
+  
+  listingData[index] = updated;
+  return NextResponse.json({ message: 'Updated', listing: updated });
 }
